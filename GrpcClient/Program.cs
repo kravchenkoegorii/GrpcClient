ba980c08b56a35e7951d6f43112c1a77677f1302
kravchenkoegorii <kravchenko13138@gmail.com>
2022-07-29 14:58:32 +0300

    first commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            using var call = client.SayHelloStream();
+
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var response in call.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(response.Message);
+                }
+            });
+
+            while(true)
+            {
+                var result = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(result))
+                {
+                    break;
+                }
+
+                await call.RequestStream.WriteAsync(new HelloRequest() { Name = result });
+            }
+
+            await call.RequestStream.CompleteAsync();
+            await readTask;
+
+            Console.ReadKey();
+        }
+    }
+}
